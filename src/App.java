2b1ed8af1ff4739d2899d8f77f10b76aacf4fc8b
devkala48 <49695338+devkala48@users.noreply.github.com>
2023-10-11 17:50:06 +0000

    array declare, memory allocate and initalize

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 public class App {
   
   public static void main(String[] args) {
-    
+    //double[] lotteryNums; array of double type declare only
+    // double[] lotteryNums = new double[5]; declare and allocate memory of size 5
+    double[] lotteryNums = {10, 11, 12, 13, 14}; //declare, memory allocate and initalize all in one line
+    System.out.println(lotteryNums[2]);
+    lotteryNums[2] = 15;
+    System.out.println(lotteryNums[2]);
   }
 }
