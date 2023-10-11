a1c383cdcd14b6ff5d8e42fc18810de813f33406
devkala48 <49695338+devkala48@users.noreply.github.com>
2023-10-11 03:22:10 +0000

    iterate through an array

--- src/App.java
+++ src/App.java
@@ -1,6 +1,28 @@
+import java.util.Arrays;
+
 public class App {
   
   public static void main(String[] args) {
     int[] arr = new int[]{92, 12, 38, 74};
+    //printing each items of an array
+    for (int i = 0; i < arr.length; i++) {
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+    //printing each items of an array in reverse order 
+    for (int i = arr.length -1; i >= 0; i--) {
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    //printing every other items of an array
+    for (int i = arr.length - 1; i >= 0; i = i - 2) {
+      System.out.println("array index : " + i);
+      System.out.println(arr[i]);
+    }
+    System.out.println();
+
+    //Java Stream, converting an array to stream
+    Arrays.stream(arr).forEach(System.out::println);
   }
 }
